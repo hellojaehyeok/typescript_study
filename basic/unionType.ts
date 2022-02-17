bafb858084d0045b5525aee2eae898746c9016a3
Song jae hyeok <wakij6587@naver.com>
2022-02-17 23:02:06 +0900

    인터페이스 유니온타입/인터섹션타입 정리

--- basic/unionType.ts
+++ basic/unionType.ts
@@ -16,3 +16,29 @@ function logMesage(value:string | number){
 logMesage("hello");
 logMesage(10);
 
+
+
+interface Developer {
+    name:string;
+    skill:string;
+}
+interface Person{
+    name:string;
+    age:number;
+}
+
+// 두개의 인터페이스를 유니온타입으로 정의하면 공통적인 타입만 사용가능하다.
+function askSomeone(someone:Developer | Person){
+    console.log(someone.name)
+}
+askSomeone({name:"song", skill:"react", age:3});
+askSomeone({name:"song", age:0});
+
+
+// &는 인터섹션타입으로 Developer도 가능하고 Person도 가능한 상태이다.
+function askSomeone2(someone:Developer & Person){
+    console.log(someone.name)
+    console.log(someone.skill)
+    console.log(someone.age)
+}
+askSomeone2({name:"song", skill:"react", age:0});
