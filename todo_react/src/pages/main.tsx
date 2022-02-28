a9d6a464f3f6d2316ad98347e0cb7c64e3af6656
Song jae hyeok <wakij6587@naver.com>
2022-02-28 23:18:16 +0900

    초기 불필요코드 제거, react-router/styled-components 설치

---
+++ todo_react/src/pages/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components'
+
+const Main = ({}) => {
+    
+    return(
+        <Container>
+            To do list
+        </Container>
+    )
+};
+
+export default Main;
+
+const Container = styled.div`
+`
